7e6d7b33b3f783e45ecb1f6e3afc135c81977ae5
ReginaldWang <114448545+ReginaldWang@users.noreply.github.com>
2025-10-17 18:59:02 -0700

    Update asyncio-server.py

--- asyncio-server.py
+++ asyncio-server.py
@@ -1,39 +1,31 @@
 import asyncio
-import time
-import json
 
 SERVER_HOST = "localhost"
 SERVER_PORT = 8000
 
 async def handle_conn(reader, writer):
     addr = writer.get_extra_info("peername")
-    print(f"[SERVER] Client connected: {addr}")
+    print(f"[SERVER] Received Connection from {addr}")
 
     while True:
         data = await reader.read(1024)
         if not data:
-            print(f"[SERVER] Client {addr} disconnected")
+            print("[SERVER] Connection Closed")
             break
 
-        try:
-            request = json.loads(data.decode())
-        except json.JSONDecodeError:
-            request = {}
+        msg = data.decode()
+        print(f"[SERVER] Message: {data.decode()}")
 
-        if request.get("type") == "time req":
-            server_time = time.time()  
-            response = json.dumps({"type": "time resp", "server_time": server_time})
-            writer.write(response.encode())
-            await writer.drain()
+        writer.write(b"Message Received!")
 
 async def main():
-    server = await asyncio.start_server(handle_conn, SERVER_HOST, SERVER_PORT)
-    addr = server.sockets[0].getsockname()
-    print(f"[SERVER] Listening on {SERVER_HOST}:{SERVER_PORT}")
+    srv = await asyncio.start_server(handle_conn, SERVER_HOST, SERVER_PORT)
+    addr = srv.sockets[0].getsockname()
 
-    async with server:
-        await server.serve_forever()
+    print(f"[SERVER] ### SERVER LISTENING ON {SERVER_HOST}:{SERVER_PORT} ###")
 
+    async with srv:
+        await srv.serve_forever()
 
 if __name__ == "__main__":
     asyncio.run(main())
